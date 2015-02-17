6c7942e028960a1e4a3979124fab116258c1af4e
Nick Nance <nance.nick@gmail.com>
2015-02-17 12:58:25 -0600

    initial project setup

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    mocha_phantomjs: {
+      options: {
+        reporter: "spec"
+      },
+      plugin: {
+        files: {
+          src: ['test/spec.html']
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc : '.jshintrc'
+      },
+      plugin : [ 'backbone.unloader.js' ]
+    },
+
+    watch: {
+      plugin : {
+        files : ['backbone.unloader.js', 'test/**/*.js', 'test/spec.html'],
+        tasks : ['jshint', 'mocha_phantomjs:plugin']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['jshint', 'mocha_phantomjs:plugin']);
+
+  grunt.registerTask('dev', ['test', 'watch:plugin']);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'mocha_phantomjs:plugin', 'preprocess', 'template', 'concat', 'uglify']);
+
+};
---
+++ backbone.unloader.js
@@ -0,0 +1,57 @@
+/*!
+* backbone.unloader.js
+* Copyright 2014, Nick Nance (@nancenick) and ConnectSolutions (@ConnectSolutns)
+* backbone.unloader.js may be freely distributed under the MIT license.
+*/
+(function (factory) {
+
+  // Check for AMD.
+  if (typeof define === 'function' && define.amd) {
+    define(['underscore', 'backbone', 'exports']);
+  }
+
+  // Next for Node.js or CommonJS.
+  else if (typeof exports === 'object') {
+    factory(require('underscore'), require('backbone'));
+  }
+
+  // Finally, as a browser global.
+  else {
+    factory(_, Backbone);
+  }
+
+}(function (_, Backbone) {
+  _.extend(Backbone.Router.prototype, {
+    loadRoutes: function(routes) {
+      if (this.routes) {
+        this.unloadRoutes();
+      }
+      // set backbone internal routes variable
+      this.routes = routes;
+      // call backbone internal route binding function
+      this._bindRoutes();
+    },
+
+    unloadRoutes: function() {
+      if (this.routes) {
+        var routes = _.keys(this.routes);
+        // turn each route into a regex expression to match what is in the handlers
+        routes.map(function(route){
+          if (!_.isRegExp(route)) {
+            route = this._routeToRegExp(route);
+          }
+          return route;
+        }.bind(this))
+        // find each router in the handlers and remove it
+        .forEach(function(route){
+          var routeIndex = _.findIndex(Backbone.history.handlers, function(handler){
+            return handler.route.toString() === route.toString();
+          });
+          Backbone.history.handlers.splice(routeIndex,1);
+        });
+        this.routes = null;
+      }
+    }
+
+  });
+}));
